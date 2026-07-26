62d8e8d7f5f35e03bf1a1be0c4f6b8c2271905d6
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-26 04:04:02 -0500

    Cargar estilos del logo y navegación

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './hero-fix.css'
+import './navigation-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
